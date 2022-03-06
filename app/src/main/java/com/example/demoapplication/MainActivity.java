cd56d05d3e5c8e2686e39e67d39633304640bb53
Yanellevdb <77410151+Yanellevdb@users.noreply.github.com>
2022-03-06 15:53:59 +0100

    Button toegevoegd

--- app/src/main/java/com/example/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/demoapplication/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mijnknop= findViewById(R.id.mijnbutton);
+        mijnknop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView mijntextview= findViewById(R.id.mijntextview);
+                mijntextview.setText("Hello Yanelle");
+            }
+        });
     }
 }
